748607a2cdc683974b7c90d294fef318e4cd4881
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-04 19:08:37 +0100

    finish PPSTRUCTUREv3 for extraction

--- src/OCR/PPSTRUCTUREv3.py
+++ src/OCR/PPSTRUCTUREv3.py
@@ -1,47 +1,32 @@
 # 1: pip install paddlepaddle-gpu==3.3.0 -i https://www.paddlepaddle.org.cn/packages/stable/cu126/
 # 2: pip install paddleocr[all] pandas ollama pydantic
+# 3: pip install opencv-python-headless==4.12.0.88 (because of a new opencv bug)
 import glob
 import os
 from paddleocr import PPStructureV3
 from time import gmtime, strftime
-from typing import List
 from llm_ollama import extract_courses
-# FIXME: correct paths as in example
-IMAGES = "/data/images"
-OUTPUT = "/data/output/PPSTRUCTUREv3"
+
+if os.path.exists("/data/images"):
+    print("Path /data/... found.")
+    # using pdfs instead of images, due to ppstructure sometimes running into a numpy error with those
+    PDFS = "/data/pdfs"  
+    OUTPUT = "/data/output/PPSTRUCTUREv3"
+else:
+    print("Path /data/ not found, terminating program!")
+    exit()
+
+if not os.path.exists(OUTPUT):
+    print(f"Creating output folder at {OUTPUT}.")
+    os.makedirs(OUTPUT)
+    os.makedirs(f"{OUTPUT}/text")
+
 LLMS = ["qwen3:0.6b", "qwen3:4b", "qwen3:14b", "qwen3:32b", "qwen3:235b"]
 
-image_paths = glob.glob(f"{IMAGES}/*.jpg")
-    
-# TODO: using max settings, decide whether to use these or to find a common ground with others
-paddleocr = PPStructureV3(
-    lang="en",
-    device="gpu",
-    ocr_version="PP-OCRv5",
-    layout_detection_model_name=,
-    chart_recognition_model_name=,
-    region_detection_model_name=,
-    doc_orientation_classify_model_name=,
-    doc_unwarping_model_name=,
-    text_detection_model_name=,
-    textline_orientation_model_name=,
-    text_recognition_model_name=,
-    table_classification_model_name=,
-    table_orientation_classify_model_name=,
-    table_orientation_classify_model_name=
-    use_doc_orientation_classify=True,
-        use_doc_unwarping=None,
-        use_textline_orientation=None,
-        use_seal_recognition=None,
-        use_table_recognition=None,
-        use_formula_recognition=None,
-        use_chart_recognition=None,
-        use_region_detection=None,
-        format_block_content=None,
-        markdown_ignore_labels=None,
-)
+pdf_paths = glob.glob(f"{PDFS}/*.pdf")
 
-PaddleOCR(
+# TODO: using same settings as with PaddleOCR, but added use_table_recognition, as we want to find out impact of structure of those
+paddleocr = PPStructureV3(
     lang="en",
     device="gpu",
     ocr_version="PP-OCRv5",
@@ -52,16 +37,20 @@ PaddleOCR(
     text_recognition_model_name="PP-OCRv5_server_rec",
     use_doc_orientation_classify=True,
     use_doc_unwarping=True,
-    use_textline_orientation=True
+    use_textline_orientation=True,
+    use_table_recognition=True
 )
 
-# run OCR over all images, use structure from PPStructureV3
+# run OCR over all pdfs, use structure from PPStructureV3
 text_dict = {}
-for image in image_paths:
-    image_name = os.path.basename(image)  #FIXME: fix to proper name extraction -> "2_25"
-    result = paddleocr.predict(image)
-
-    text_dict[image_name] = text
+for pdf in pdf_paths:
+    pdf_name = os.path.basename(pdf).split(".")[0]
+    result = paddleocr.predict(pdf)
+    md_dict = result[0].markdown
+    text = md_dict["markdown_texts"]
+    with open(f"{OUTPUT}/text/{pdf_name}.txt", "w", encoding="utf-8") as text_file:
+        text_file.write(text)
+    text_dict[pdf_name] = text
 
 # extract relevant entries with each ollama model
 for llm_model in LLMS: 
