da0891e8d2328ec41c7dec966d8436945d020568
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-05 11:06:54 +0100

    define default values for LLM if no fitting value of grades or credits can be found in given context

--- src/OCR/llm_ollama.py
+++ src/OCR/llm_ollama.py
@@ -7,13 +7,13 @@ from typing import  List, Literal
 class Course(BaseModel):
     academic_field: Literal["Computer Science", "Mathematics"]
     course_name: str
-    grade: float
-    awarded_credits: float
+    grade: float | str | None = None
+    awarded_credits: float | None = None
 
 class Courses(BaseModel):
     courses: List[Course]
 
-# TODO: Optimize prompt, +determine one specific type of "N/A"
+# TODO: optimize prompt
 def extract_courses(text, llm_model) -> pd.DataFrame:
     response: ChatResponse = chat(
         model=llm_model,
@@ -24,13 +24,14 @@ def extract_courses(text, llm_model) -> pd.DataFrame:
                     You are given the OCR result of a scanned page 
                     1. Determine if it is an academic record or transcript showing a single student's grades and/or earned credits. 
                     2. If yes, identify all courses whose subject matter falls under the academic fields of "Computer Science" or "Mathematics" — this includes courses that are clearly related even if the document does not use those exact headings. Use your judgment to classify them based on their titles, descriptions, or course codes. 
+                       If you cannot find any value for the course_name or grade default to the value "None" as specified in the schema I am about to send you.
                     Extract those using the following schema for a course:
                     ```python
                     class Course(BaseModel):
-                    academic_field: Literal["Computer Science", "Mathematics"]
-                    course_name: str
-                    grade: float
-                    awarded_credits: float
+                        academic_field: Literal["Computer Science", "Mathematics"]
+                        course_name: str
+                        grade: float | str | None = None
+                        awarded_credits: float | None = None
                     ```
                     3. If it is not an academic record or transcript, return no results.
                     Here is the OCR result: 
@@ -43,4 +44,7 @@ def extract_courses(text, llm_model) -> pd.DataFrame:
 
     data = json.loads(response["message"]["content"])
     df = pd.DataFrame(data["courses"])
+    # fill empty cells (None) with "N/A"
+    df['grade'] = df['grade'].fillna("N/A")
+    df['awarded_credits'] = df['awarded_credits'].fillna("N/A")
     return df
