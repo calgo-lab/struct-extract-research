2c73c22f3cdf4df45bbe6a347588c566e28512a1
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-09 13:35:52 +0100

    update vlm running script

--- src/VLM/vlms.py
+++ src/VLM/vlms.py
@@ -2,8 +2,9 @@
 import base64
 import glob
 import os
+import subprocess
 from pathlib import Path
-from time import gmtime, strftime
+from time import gmtime, strftime, sleep
 from vlm_ollama import extract_courses
 
 if os.path.exists("/data/images"):
@@ -15,26 +16,23 @@ else:
     exit()
 
 # models and context sizes
-VLMS = {
-    "gemma3:27b": 128000,
-    "ministral-3:14b": 256000,
-    "deepseek-ocr:3b": 8000,
-    "llava:7b": 32000,
-    "qwen2.5vl:32b": 125000,
-    "qwen3-vl:32b": 256000
-}
+VLMS = ["gemma3:27b", "ministral-3:14b", "deepseek-ocr:3b", "llava:7b", "qwen2.5vl:32b", "qwen3-vl:32b"]
 
 image_paths = glob.glob(f"{IMAGES}/*.jpg")
 
-for vlm_model, context_size in VLMS.items():
-    vlm_output = f"{OUTPUT}/{vlm_model.replace(':', '_')}"
+for vlm_model in VLMS:
+    vlm_output = f"{OUTPUT}/{vlm_model.replace(':', '_')}_rt"
     print(f"Creating output folder at {vlm_output}.")
     os.makedirs(f"{vlm_output}", exist_ok=True)
 
     for i, image in enumerate(image_paths):
         image_name = os.path.basename(image).split(".")[0] 
-        print(f"Image {i}: {image_name} - {strftime('%Y-%m-%d %H:%M:%S', gmtime())}")
+        print(f"Image {i}: {image_name}; VLM: {vlm_model} - {strftime('%Y-%m-%d %H:%M:%S', gmtime())}")
         # ollama expects a base64 encoded image string
         image_b64 = base64.b64encode(Path(image).read_bytes()).decode()
-        df = extract_courses(image_b64, vlm_model, context_size)
+        df = extract_courses(image_b64, vlm_model)
         df.to_csv(f"{vlm_output}/{image_name}.csv")
+
+    # end one model before starting the next
+    subprocess.run(["ollama", "stop", f"{vlm_model}"])
+    sleep(30)
