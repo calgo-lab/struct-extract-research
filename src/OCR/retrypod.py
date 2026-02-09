482fee1bb17107cde635c88d094df1c9b75a77f7
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-09 09:49:22 +0100

    fix retrypod

--- src/OCR/retrypod.py
+++ src/OCR/retrypod.py
@@ -1,26 +1,31 @@
-import glob
 import os
 from llm_ollama import extract_courses
+from pathlib import Path
 from time import gmtime, strftime
 
-ocr_model_name = "EasyOCR"  # TODO: change accordingly
-OUTPUT = f"/data/output/{ocr_model_name}"
-LLM = "qwen3:235b"  # TODO: change accordingly
+OCR_NAMES = ["docTR", "EasyOCR", "MinerU", "PPOCRv5", "PPStructureV3", "Pytesseract"]  
+OUTPUT = f"/data/output"
+LLM = "qwen3:0.6b"  # TODO: change accordingly
 
-text_dict = {}
-text_path = f"/data/output/{ocr_model_name}/text"
-text_paths = glob.glob(f"{text_path}/*.txt")
-for text in text_paths:
-    text_name = os.path.basename(text).split(".")[0]
-    print(text_name)
-    text_dict[text_name] = text
-
-print(f"LLM: {LLM} start. - {strftime('%Y-%m-%d %H:%M:%S', gmtime())}")
-params = LLM.split(":")[1]
-if not os.path.exists(f"{OUTPUT}/{params}_rt"):
-    os.makedirs(f"{OUTPUT}/{params}")
-for name, text in text_dict.items(): 
-    df = extract_courses(text, LLM)
-    df.to_csv(f"{OUTPUT}/{params}/{name}.csv")
-print(f"LLM: {LLM} done. - {strftime('%Y-%m-%d %H:%M:%S', gmtime())}")
+for ocr_name in OCR_NAMES:
+    ocr_output = f"{OUTPUT}/{ocr_name}"
+    text_dict = {}
+    text_path = Path(f"/data/output/{ocr_name}/text")
+    # get all texts
+    for path in text_path.glob("*.txt"):
+        text_name = path.stem
+        with path.open("r", encoding="utf-8") as f:
+            text = f.read()
+        text_dict[text_name] = text
+    print(text_dict)  # check if that works!
+    
+    print(f"LLM: {LLM}; OCR: {ocr_name} start. - {strftime('%Y-%m-%d %H:%M:%S', gmtime())}")
+    params = LLM.split(":")[1]
+    llm_output = f"{ocr_output}/{params}_rt"
+    if not os.path.exists(llm_output):
+        os.makedirs(llm_output)
+    for name, text in text_dict.items(): 
+        df = extract_courses(text, LLM)
+        df.to_csv(f"{llm_output}/{name}.csv")
+    print(f"LLM: {LLM}; OCR: {ocr_name} done. - {strftime('%Y-%m-%d %H:%M:%S', gmtime())}")
     
