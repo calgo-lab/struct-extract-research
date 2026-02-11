a19e5dc6ce56a218cf4a27172b0b023b7943db1d
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-11 17:45:16 +0100

    update eval, add config options, merge with eval_all

--- src/eval.py
+++ src/eval.py
@@ -1,11 +1,48 @@
 # Script that evaluates the similarity of the .csv output of any tested model and the ground-truth .csv using data science typical metrics.
+import glob
+import os
 import pandas as pd 
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import precision_score, recall_score, f1_score, jaccard_score
 
-output = "output/test.csv"
-gt = "data/eval/2_9.csv"
+OUTPUT_FOLDER = "data/output"
+GROUND_TRUTH_PATHS = {
+    os.path.basename(path): path 
+    for path in glob.glob("data/ground_truth/*.csv")
+}
+OCR = ["docTR", "EasyOCR", "MinerU", "PPOCRv5", "PPStructureV3", "Pytesseract"]
+OCR_PARAMS = ["0.6b", "4b", "14b", "32b", "235b"]
+# name: path
+VLM = {
+    "Gemma 3": "gemma3_27b",
+    "LLava": "llava_7b",
+    "Ministral 3": "ministral-3_14b",
+    "Qwen2.5-VL": "qwen2.5vl_32b",
+    "Qwen3-VL": "qwen3-vl_32b"
+}
 
-# helper function
+# set output floating point format
+pd.options.display.float_format = '{:.3f}'.format
+
+## -- helper functions -- 
+# normalize (with the goal to evaluate if all the correct data has been extracted, NOT to evaluate if stuff like capitalization is correct!) 
+def normalize_df(df, columns_to_drop):
+    # drop index column/unnamed columns
+    df.drop(df.columns[df.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
+    # strip leading/trailing spaces
+    for col in ['academic_field', 'course_name', 'grade', 'awarded_credits']:
+        df[col] = df[col].astype(str).str.strip()    
+    # academic_field and course_name to lowercase
+    df['academic_field'] = df['academic_field'].str.lower()
+    df['course_name'] = df['course_name'].str.lower()
+    # if None was sent as a String it hasn't been replaced with "N/A" like `None` yet (check `llm/vlm_ollama.py`)
+    df['grade'] = df['grade'].replace("None", "N/A")
+    df['awarded_credits'] = df['awarded_credits'].replace("None", "N/A")
+    # normalize possibly numerical values to floats, if NaN, just use the (lowercase) string
+    df['grade'] = df['grade'].apply(normalize_number)
+    df['awarded_credits'] = df['awarded_credits'].apply(normalize_number)
+    df = df.drop(columns=columns_to_drop)
+    return df
+
 def normalize_number(x):
     # grade: numeric -> float, letter -> lowercase
     try:
@@ -13,57 +50,144 @@ def normalize_number(x):
     except (ValueError, TypeError):
         return str(x).lower()
     
-# read in
-output_df = pd.read_csv(output)
-gt_df = pd.read_csv(gt)
-df_list = [output_df, gt_df]
+def create_evaluation(df_gt, df_output):
+    # create tokens out of dataframes
+    #  -> a token is one row for academic_field, course_name, grade and awarded_credits
+    gt_token = set(df_gt.itertuples(index=False, name=None))
+    output_token = set(df_output.itertuples(index=False, name=None))
+    all_token = list(gt_token | output_token)
 
-# normalize (with the goal to evaluate if all the correct data has been extracted, NOT to evaluate if stuff like capitalization is correct!) 
-# TODO: check if thats okay
-for df in df_list:
-    # drop index column
-    df.drop(df.columns[df.columns.str.contains(
-        'unnamed', case=False)], axis=1, inplace=True)
-    # strip leading/trailing spaces
-    for col in ['academic_field', 'course_name', 'grade', 'awarded_credits']:
-        df[col] = df[col].astype(str).str.strip()    
-    # academic_field and course_name to lowercase
-    df['academic_field'] = df['academic_field'].str.lower()
-    df['course_name'] = df['course_name'].str.lower()
-    # normalize possibly numerical values to floats, if NaN, just use the string
-    df['grade'] = df['grade'].apply(normalize_number)
-    df['awarded_credits'] = df['awarded_credits'].apply(normalize_number)
-    # TODO: define and enforce default for N/A values
-    df = df.fillna("N/A")  # temp filling of empty cells, doesn't take care of N/A placeholders
-    # <CODE HERE>
+    if len(all_token) == 0:
+        return {
+            "Precision": 0,
+            "Recall": 0,
+            "F1": 0,
+            "Jaccard": 0
+        }
+    
+    y_true = [1 if row in gt_token else 0 for row in all_token]
+    y_pred = [1 if row in output_token else 0 for row in all_token] 
+    # TP: y_true[i] == 1, y_pred[i] == 1
+    # FP: y_true[i] == 0, y_pred[i] == 1
+    # FN: y_true[i] == 1, y_pred[i] == 0
+    precision = precision_score(y_true, y_pred, zero_division=0)
+    recall = recall_score(y_true, y_pred, zero_division=0)
+    f1 = f1_score(y_true, y_pred, zero_division=0)
+    jc = jaccard_score(y_true, y_pred, zero_division=0)
+    return {
+        "Precision": precision,
+        "Recall": recall,
+        "F1": f1,
+        "Jaccard": jc
+    }
 
-# evaluate 
-# FIXME: check again if sets are the way to go - each row SHOULD only exist once, does it really though; is a check and notification if it doesnt enough?
-# idea: use list for gt and delete elements if match exists while iterating through predictions! 
-gt_rows = set(gt_df.itertuples(index=False, name=None))
-pred_rows = set(output_df.itertuples(index=False, name=None))
-all_rows = list(gt_rows | pred_rows)
-y_true = [1 if row in gt_rows else 0 for row in all_rows]
-y_pred = [1 if row in pred_rows else 0 for row in all_rows]
-
-precision = precision_score(y_true, y_pred)
-recall = recall_score(y_true, y_pred)
-f1 = f1_score(y_true, y_pred)
-# pretty print results
-results = {
-    "Metric": [
-        "Precision",
-        "Recall",
-        "F1"
-    ],
-    "Value": [
-        precision,
-        recall,
-        f1
-    ]
+# -- main program --
+eval_configs = {
+    "all": {
+        "exclude_cols": []
+    },
+    "only_courses": {
+        "exclude_cols": ["academic_field", "grade", "awarded_credits"]
+    },
+    "no_credits": {
+        "exclude_cols": ["awarded_credits"]
+    },
+    "no_grades": {
+        "exclude_cols": ["grade"]
+    }
 }
 
-df = pd.DataFrame(results)
-df["Value"] = df["Value"].map(lambda x: f"{x:.2f}")
+ocr_results_storage = {name: [] for name in eval_configs.keys()}
+vlm_results_storage = {name: [] for name in eval_configs.keys()}
 
-print(df.to_string(index=False))
+# EVAL OCR
+for ocr_engine in OCR:
+    for params in OCR_PARAMS:
+        # "output" means the LLMs/VLMs output
+        output_paths = {
+            os.path.basename(path): path 
+            for path in glob.glob(f"{OUTPUT_FOLDER}/{ocr_engine}/{params}/*.csv")
+        }
+        param_results = {name: [] for name in eval_configs.keys()}
+        for file_name in GROUND_TRUTH_PATHS.keys():
+            for name, config in eval_configs.items():
+                # create normalized dataframe out of csv
+                df_gt = normalize_df(pd.read_csv(GROUND_TRUTH_PATHS[file_name]), config["exclude_cols"])
+                df_output = normalize_df(pd.read_csv(output_paths[file_name]), config["exclude_cols"])
+                # run evaluation (Precision, Recall, F1, Jaccard)
+                results = create_evaluation(df_gt, df_output)
+                param_results[name].append(results)
+
+        # calculate mean values
+        for name in eval_configs.keys():
+            tmp_df = pd.DataFrame(param_results[name])
+            mean_results = {
+                "OCR Engine": ocr_engine,
+                "Model": f"Qwen3:{params}",
+                "Precision": tmp_df["Precision"].mean(),
+                "Recall": tmp_df["Recall"].mean(),
+                "F1": tmp_df["F1"].mean(),
+                "Jaccard": tmp_df["Jaccard"].mean()
+            }
+            ocr_results_storage[name].append(mean_results)
+
+# EVAL VLM
+for vlm_name, vlm_dir_name in VLM.items():
+    # "output" means the LLMs/VLMs output
+    output_paths = {
+        os.path.basename(path): path 
+        for path in glob.glob(f"{OUTPUT_FOLDER}/{vlm_dir_name}/*.csv")
+    }
+    param_results = {name: [] for name in eval_configs.keys()}
+    for file_name in GROUND_TRUTH_PATHS.keys():
+        for name, config in eval_configs.items():
+            # create normalized dataframe out of csv
+            df_gt = normalize_df(pd.read_csv(GROUND_TRUTH_PATHS[file_name]), config["exclude_cols"])
+            df_output = normalize_df(pd.read_csv(output_paths[file_name]), config["exclude_cols"]) 
+            # run evaluation (Precision, Recall, F1, Jaccard)
+            results = create_evaluation(df_gt, df_output)
+            param_results[name].append(results)
+        
+    # calculate mean values
+    for name in eval_configs.keys():
+        tmp_df = pd.DataFrame(param_results[name])
+        mean_results = {
+            "Model": f"{vlm_name}",
+            "Precision": tmp_df["Precision"].mean(),
+            "Recall": tmp_df["Recall"].mean(),
+            "F1": tmp_df["F1"].mean(),
+            "Jaccard": tmp_df["Jaccard"].mean()
+        }
+        vlm_results_storage[name].append(mean_results)
+
+print("OCR: ALL")
+ocr_result_all_df = pd.DataFrame(ocr_results_storage["all"])
+print(ocr_result_all_df)
+print("="*60)
+print("OCR: COURSES")
+ocr_result_only_courses_df = pd.DataFrame(ocr_results_storage["only_courses"])
+print(ocr_result_only_courses_df)
+print("="*60)
+print("OCR: NO CREDITS")
+ocr_result_no_credits_df = pd.DataFrame(ocr_results_storage["no_credits"])
+print(ocr_result_no_credits_df)
+print("="*60)
+print("OCR: NO GRADES")
+ocr_result_no_grades_df = pd.DataFrame(ocr_results_storage["no_grades"])
+print(ocr_result_no_grades_df)
+print("="*60)
+print("VLM: ALL")
+vlm_result_all_df = pd.DataFrame(vlm_results_storage["all"])
+print(vlm_result_all_df)
+print("="*60)
+print("VLM: COURSES")
+vlm_result_only_courses_df = pd.DataFrame(vlm_results_storage["only_courses"])
+print(vlm_result_only_courses_df)
+print("="*60)
+print("VLM: NO CREDITS")
+vlm_result_no_credits_df = pd.DataFrame(vlm_results_storage["no_credits"])
+print(vlm_result_no_credits_df)
+print("="*60)
+print("VLM: NO GRADES")
+vlm_result_no_grades_df = pd.DataFrame(vlm_results_storage["no_grades"])
+print(vlm_result_no_grades_df)
