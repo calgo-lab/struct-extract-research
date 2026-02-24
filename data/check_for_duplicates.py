c9620c19a8e7e834dee08888ab1a923ad5c06cfe
Eli <134539241+Elinteger@users.noreply.github.com>
2026-02-24 16:37:05 +0100

    add check for total amount of rows

--- data/check_for_duplicates.py
+++ data/check_for_duplicates.py
@@ -3,15 +3,18 @@
 import glob
 import pandas as pd
 
-eval_data_path = "data/eval"
+eval_data_path = "data/ground_truth"
 eval_data = glob.glob(f"{eval_data_path}/*.csv")
 
 dup_count = 0
+total_rows = 0
 
-for path in eval_data[:1]:
+for path in eval_data:
     df = pd.read_csv(path)
     for dup_bool in df.duplicated():
         if dup_bool == True:
             dup_count +=1 
+    total_rows += df.shape[0]
 
 print(f"Ground Trouth holds {dup_count} duplicate \"Token\" (rows).")
+print(f"There are a total of {total_rows} rows.")
